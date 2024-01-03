01b7dc48e34e93e96526a4e558e7e301830b5421
Shaneel Kumar <shaneel_kumar@live.com>
2024-01-03 19:14:49 +1100

    add ticking clock (countdown)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, useState } from "react";
+import { Dispatch, useEffect, useState } from "react";
 import clsx from "clsx";
 
 function App() {
@@ -12,11 +12,11 @@ function App() {
 
   return (
     <>
-      <header className="text-light-grey bg-purp absolute top-0 left-0 w-full">
-        <h1 className="text-4xl">pomodoro</h1>
+      <header className="text-light-grey bg-purp absolute top-0 left-0 w-full mt-10">
+        <h1 className="text-4xl text-center">pomodoro</h1>
       </header>
 
-      <main className="text-white bg-purp h-dvh pt-20">
+      <main className="flex flex-col items-center gap-10 text-white bg-purp h-dvh pt-32">
         <nav className="flex items-center justify-items-end rounded-full bg-purp-dark w-fit p-2">
           {buttons.map((button) => (
             <PomodoroButton
@@ -40,11 +40,39 @@ function App() {
 }
 
 function Timer() {
+  const [time, setTime] = useState(100);
+
+  useEffect(() => {
+    if (time === 0) return;
+
+    const interval = setInterval(() => {
+      setTime(time - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [time]);
+
+  const generateClockText = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    let clockTime = "";
+
+    clockTime += minutes < 10 ? "0" + minutes : minutes;
+    clockTime += ":";
+    clockTime += seconds < 10 ? "0" + seconds : seconds;
+
+    return clockTime;
+  };
+
+  const clockTime = generateClockText();
+
   return (
     <div
-      className={`flex items-center justify-center w-[500px] h-[500px] bg-gradient-to-br from-purp-dark to-[rgba(255,255,255,0.1)] rounded-full shadow-[-35px_-35px_50px_rgba(255,255,255,0.04),35px_35px_50px_rgba(0,0,0,0.2)]`}
+      className={`flex items-center justify-center w-[400px] aspect-square bg-gradient-to-br from-purp-dark to-[rgba(255,255,255,0.1)] rounded-full shadow-[-35px_-35px_50px_rgba(255,255,255,0.04),35px_35px_50px_rgba(0,0,0,0.2)]`}
     >
-      <div className="w-[445px] aspect-square bg-purp-dark rounded-full"></div>
+      <div className="w-[345px] aspect-square bg-purp-dark rounded-full flex items-center justify-center">
+        <h2 className="text-6xl text-light-grey">{clockTime}</h2>
+      </div>
     </div>
   );
 }
